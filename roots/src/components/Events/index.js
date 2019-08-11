99c048dbe9c8550b7b6d4da4f4097fea2a4763de
YordaniBonilla <b.yordani93@gmail.com>
2019-08-10 21:32:52 -0500

    create fetch api to get data from my other project endpoint running on alternate port

--- roots/src/components/Events/index.js
+++ roots/src/components/Events/index.js
@@ -14,9 +14,21 @@ class Events extends Component {
                 name: 'peda kodemia',
                 location: 'en mi casa',
                 date: '2019-07-18',
+                items: [{},{},{}]
             }]
         }
     }
+
+    async componentDidMount() {
+        const response = await fetch('http://localhost:3000/events');
+
+        const { data = {}} = await response.json()
+
+        const { events } = data;
+
+        this.setState({ events});
+    }
+
     render() {
       return this.state.events.map((event) => (
         <div  classNames={styles.Event}>
@@ -29,11 +41,11 @@ class Events extends Component {
             </Card.Header>
             <ListGroup variant="flush">
               {
-                  event.items.map((item)=> (
-                      <ListGroup.Item>
-                          <div>{ item.quantity} * { item.name }</div>
-                          <div>quien lo va a llevar: { item.carrier }</div>
-                      </ListGroup.Item>    
+                event.items.map((item)=> (
+                    <ListGroup.Item>
+                        <div>{ item.quantity} * { item.name }</div>
+                        <div>quien lo va a llevar: { item.carrier }</div>
+                    </ListGroup.Item>    
                   ))
               }
             </ListGroup> 
